28c302943673e5dfc987385eea30868fef0394a8
rajib2das <rajib2das@gmail.com>
2018-07-20 07:36:39 -0400

    first file add

---
+++ arraypractice.php
@@ -0,0 +1,15 @@
+<Doctype HTML>
+  <head>
+ <meta charset="utf-8">
+    </head>
+  <title>
+   Array and its functions practice
+  </title>
+  <body>
+    <?php 
+    $alpha=66;
+    $beta=99;
+    echo "the sum is ".$alpha+$beta;
+    
+    ?>
+  </body>
